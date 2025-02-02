ac97da8dfa66833e300cf49c3a4d0bb97cdb5bf7
Shymurskyy <shymurskyy@interia.pl>
2025-02-02 14:37:50 +0100

    Initail commit

---
+++ LoyAll/LoyAll/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace LoyAll
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
