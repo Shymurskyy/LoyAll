a1c8f84ef79a02a2e17f39af74eb734b89f3442b
Shymurskyy <shymurskyy@interia.pl>
2025-02-06 17:56:55 +0100

    Working barcode with dynamic height and width

--- LoyAll/LoyAll/Views/CardDetailPage.xaml.cs
+++ LoyAll/LoyAll/Views/CardDetailPage.xaml.cs
@@ -1,21 +1,41 @@
 using LoyAll.Model;
 using QRCoder;
 using SkiaSharp;
+using ZXing;
+using ZXing.SkiaSharp;
 
 namespace LoyAll
 {
     public partial class CardDetailPage : ContentPage
     {
+        private readonly Card _card;
+
         public CardDetailPage(Card card)
         {
             InitializeComponent();
-            StoreNameLabel.Text = card.StoreName;
-            CardImage.Source = ImageSource.FromFile(card.CardValue);
+            _card = card;
 
-            if (!string.IsNullOrEmpty(card.CardValue))
-                QrCodeImage.Source = GenerateQrCode(card.CardValue);
+            StoreNameLabel.Text = _card.StoreName;
+            CardImage.Source = ImageSource.FromFile(_card.CardValue);
+
+            // Domyœlnie pokazujemy QR Code
+            CodeImage.Source = GenerateQrCode(_card.CardValue);
+        }
+
+        private void OnBarcodeSwitchToggled(object sender, ToggledEventArgs e)
+        {
+            if (e.Value)
+            {
+                CodeImage.Source = GenerateBarcode(_card.CardValue);
+                CodeImage.WidthRequest = 400; 
+                CodeImage.HeightRequest = 400;
+            }
             else
-                QrCodeImage.Source = GenerateQrCode("Brak danych");
+            {
+                CodeImage.Source = GenerateQrCode(_card.CardValue);
+                CodeImage.WidthRequest = 200;
+                CodeImage.HeightRequest = 200;
+            }
         }
 
         public static ImageSource GenerateQrCode(string value, int size = 200)
@@ -33,5 +53,29 @@ namespace LoyAll
                 return ImageSource.FromStream(() => stream);
             }
         }
+
+        public static ImageSource GenerateBarcode(string value)
+        {
+            var writer = new BarcodeWriter
+            {
+                Format = BarcodeFormat.CODE_128,
+                Options = new ZXing.Common.EncodingOptions
+                {
+                    Width = 1600,
+                    Height = 400,
+                    Margin = 40
+                }
+            };
+
+            var bitmap = writer.Write(value);
+
+            using (var image = SKImage.FromBitmap(bitmap))
+            using (var data = image.Encode(SKEncodedImageFormat.Png, 100))
+            {
+                MemoryStream stream = new MemoryStream(data.ToArray());
+                return ImageSource.FromStream(() => stream);
+            }
+        }
+
     }
 }
